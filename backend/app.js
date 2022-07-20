8a7bfbbbbb27d78a1340da6ecba6ed377a67e3b4
Pablo Penalva <penalva.pablo@gmail.com>
2022-07-20 16:53:03 -0300

    view ejs removed

--- backend/app.js
+++ backend/app.js
@@ -14,9 +14,6 @@ const port = process.env.PORT || 3000
 const app = express()
 app.use(cors())
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
 
 app.use(logger('dev'))
 app.use(express.json())
