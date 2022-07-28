bb46d341ccf44fc0dabe4c2349543e45b9aac685
Pablo Penalva <penalva.pablo@gmail.com>
2022-07-28 11:41:42 -0300

    axiosClient

---
+++ frontend/src/Components/axiosClient.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const axiosClient = axios.create();
+
+axiosClient.defaults.baseURL = 'http://localhost:8080/api';
+
+axiosClient.defaults.headers = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+  Authorization: `Bearer ${localStorage.getItem('TOKEN')}`,
+};
+
+export default axiosClient;
+
+export async function getRequest(URL) {
+  const res = await axiosClient.get(`/${URL}`)
+  console.log(res)
+  return res
+}
+
+export async function postRequest(URL, payload) {
+  const res = await axiosClient.post(`/${URL}`, payload)
+  console.log(res)
+  return res;
+}
+
+export async function patchRequest(URL, payload) {
+  return axiosClient.patch(`/${URL}`, payload).then(response => response);
+}
+
+export async function deleteRequest(URL) {
+  return axiosClient.delete(`/${URL}`).then(response => response);
+}
+
