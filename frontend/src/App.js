3246eccdd7ded4f0a8f1fad68c83188b1005e6c5
Pablo Penalva <penalva.pablo@gmail.com>
2022-07-28 11:42:08 -0300

    react query added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,32 @@
 import './App.css';
 import { LoginForm } from './Components/LoginForm';
 import { SignForm } from './Components/SignForm';
+import { MovementsContainer } from './Components/MovementsContainer';
 import { UIProvider } from './Components/Context/UIContext';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <UIProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginForm />} />
-          <Route path="/register" element={<SignForm />} />
-        </Routes>
-      </BrowserRouter>
-    </UIProvider>
+    <QueryClientProvider client={queryClient}>
+      <UIProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="/register" element={<SignForm />} />
+            <Route path="/movements" element={<MovementsContainer />} />
+          </Routes>
+        </BrowserRouter>
+      </UIProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
