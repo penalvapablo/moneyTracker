a37b3a12e4d5f7b88c072013085bf427c1ee107a
Pablo Penalva <penalva.pablo@gmail.com>
2022-08-18 13:59:22 -0300

    movements container refactor

--- frontend/src/Components/MovementsContainer.js
+++ frontend/src/Components/MovementsContainer.js
@@ -1,36 +1,30 @@
-import { useQuery } from '@tanstack/react-query'
 import { LoaderView } from './Loader/Loader'
-import { getRequest } from './AxiosClient'
 import { Link, useNavigate } from 'react-router-dom'
 import { MovementsTable } from './MovementsTable'
 import { useState } from 'react'
+import { useMovementsData } from './Utils/useMovementsData'
+import { getBalance } from './Utils/getBalance'
 
 export const MovementsContainer = () => {
   const [filter, setMovementFilter] = useState("all")
   const navigate = useNavigate()
-
+  let balance
   const setMovementFilterFn = (e) => {
     setMovementFilter(e.target.textContent)
-    console.log(filter)
   }
-  const { isLoading, isError, data, isSuccess } = useQuery(['movements'], () => getRequest('movement'),
-    {
-      refetchOnWindowFocus: false,
-      retry: false,
-    }
-  )
+  const { isLoading, data, isSuccess } = useMovementsData()
+
+
 
   if (isLoading) {
     return <LoaderView />
   }
-  if (isError) {
-    console.log(isError)
-  }
-  if (isError && !localStorage.getItem('TOKEN')) {
+
+  if (!localStorage.getItem('TOKEN')) {
     navigate('/')
   }
   if (isSuccess) {
-
+    balance = getBalance(data)
     return (
       <div className='bg-violet-200 min-h-screen container mx-auto flex flex-col'>
         <h1 className='text-center text-2xl text-white my-4'>Movements</h1>
@@ -40,12 +34,15 @@ export const MovementsContainer = () => {
         <Link to='/categories'>
           <button className="px-4 py-1 mt-4 text-sm bg-white block my-0 mx-auto font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent" >Categories</button>
         </Link>
+        <Link to='/home'>
+          <button className="px-4 py-1 mt-4 text-sm bg-white block my-0 mx-auto font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent" >Back</button>
+        </Link>
         <div className='flex h-12 align-middle'>
           <button className={`capitalize px-4 py-1 mt-4 text-sm bg-white block my-0 mx-auto font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent ${(filter === "all") && "bg-purple-600 text-white"}`} onClick={setMovementFilterFn}>all</button>
           <button className={`capitalize px-4 py-1 mt-4 text-sm bg-white block my-0 mx-auto font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent ${(filter === "income") && "bg-purple-600 text-white"}`} onClick={setMovementFilterFn}>income</button>
-          <button className={`capitalize px-4 py-1 mt-4 text-sm bg-white block my-0 mx-auto font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent ${(filter === "expenses") && "bg-purple-600 text-white"}`} onClick={setMovementFilterFn}>Expenses</button>
+          <button className={`capitalize px-4 py-1 mt-4 text-sm bg-white block my-0 mx-auto font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent ${(filter === "expenses") && "bg-purple-600 text-white"}`} onClick={setMovementFilterFn}>expenses</button>
         </div>
-
+        <p className='text-center mt-4'>balance: {balance}</p>
         <MovementsTable data={data} filter={filter} />
       </div>
     )
